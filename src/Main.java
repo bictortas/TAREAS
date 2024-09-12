338144f7ca6e9b5c3d6926f9da3118e7576b2408
Victor <l24121359@morelia.tecnm.mx>
2024-09-11 23:02:22 -0600

    agregué la tarea 3 de la unidad 2

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Sección 1 (Información de un libro favorito)
+         */
+        String titulo = "Las ventajas de ser invisible";
+        String autor =  "Stephen Chbosky";
+        int anoPubli = 1999;
+        String genero = "Drama,Novela Juvenil";
+        int paginas = 264;
+
+        System.out.println( "Mi libro favorito es:" + titulo);
+        System.out.println( "El autor es:" + autor);
+        System.out.println( "El género es:" + genero);
+        System.out.println( "Tiene:" + paginas +  "páginas");
+
+        /*
+        Sección 2 (Información de un país que deseo visitar)
+         */
+
+        String nombrePais = "Japón";
+        String capital = "Tokio";
+        String idioma = "Japonés";
+        int habitantes = 121;
+        String moneda = "Yen";
+
+        System.out.println("Quiero visitar:" + nombrePais);
+        System.out.println("La capital de Japón es:" + capital);
+        System.out.println("El idioma oficial es:" + idioma);
+        System.out.println("Su moneda oficial es:" + moneda);
+
+        /*
+        Sección 3 (Información de un personaje histórico)
+         */
+
+        String nombrePerson = "Martin Luther King";
+        String fecha = "15/01/1929";
+        String nacion = "Estadounidense";
+        String ocupacion = "Ministro y Activista";
+        String logro = "recibió el Premio Nobel de la Paz por su activismo en pro de los derechos civiles y la justicia social";
+
+        System.out.println("Me agrada mucho " + nombrePerson);
+        System.out.println("Nació el : " + fecha);
+        System.out.println("Era de nacionalidad  " + nacion);
+        System.out.println("Era un  " + ocupacion);
+        System.out.println("Logro destacado : " + logro);
+
+        /*
+        Fin
+         */
+    }
+}

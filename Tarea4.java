dac7d9ebbcbd87c1fc18a4a5e4bfff94832e8019
Victor <l24121359@morelia.tecnm.mx>
2024-09-12 22:58:21 -0600

    Tarea 4

---
+++ Tarea4.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        Fórmula 1
+         */
+
+        double divisor = ((12 + 8) * (5 - 3)) - (6 / 2);
+        double dividendo = (3 * 4) + (8 / 2);
+
+        System.out.println(divisor / dividendo);
+
+      /*
+      Fórmula 2
+       */
+
+
+        double divisorDos = (((9 * 5) - (15 + 3)+(8 * 2) - (12 / 4)) * (7 + 2) - (6 * 3/2)) / (2 + (3 * 2) / (4 - 2)) + (5 * ((8/4) + 1) - (6 / (2 + 1)));
+        double dividendoDos = (((4 + 2) * (7 - 1)) / (3 + 1) + ((3 * 2) - (5 + 1 )) * (8 / 2)) / (3 + ((6 / 2)+ (7 / 2))) + (2 * (3 + (8 /4)));
+
+        System.out.println(divisorDos/ dividendoDos);
+
+        /*
+        Fórmula 3
+         */
+
+        double divisorTres = ((6 + 4) * 3) - (18 / 2);
+        double dividendoTres = (8 - 5) + (3 * 2);
+
+        System.out.println(divisorTres/ dividendoTres);
+
+        /*
+        FIN
+         */
+
+
+    }
+}
